732adc2edaa61054dafeb484695dc4d307fb3312
ink0rr <tomflynnbeltsazar@gmail.com>
2021-12-24 18:53:03 +0700

    Migrate to yarn

--- src/lib/src/index.ts
+++ src/lib/src/index.ts
@@ -1,5 +1,5 @@
+import fs, { mkdir } from "fs"
 import { exec } from "child_process"
-import fs from "fs"
 
 export class BedrockTS {
 	private static bedrockPaths: Array<string> = new Array<string>()
@@ -12,17 +12,21 @@ export class BedrockTS {
 		})
 	}
 
-	public static ignite() { 
+	public static ignite() {
+		mkdir('out/BP/entities', { recursive: true }, (err) => {
+			if (err) {
+				console.log(err)
+			}
+		})
 		this.bedrockPaths.forEach(path => {
-			fs.readdir(`./dist/core/${path}`, (err, files) => {
+			fs.readdir(`./src/core/${path}`, (err, files) => {
 				files.forEach(file => {
-					if (file.endsWith('.js')) {
-						exec(`node ./dist/core/${path}${file}`, (err, stdout) => {
+					if (file.endsWith('.ts')) {
+						exec(`ts-node ./src/core/${path}${file}`, (err) => {
 							if (err) {
 								console.log(err)
 								return
 							}
-							console.log(`${stdout}`)
 						})
 					}
 				})
--- src/lib/src/utils/writeJSON.ts
+++ src/lib/src/utils/writeJSON.ts
@@ -2,8 +2,7 @@ import { formatKey } from "./formatKey"
 import fs from "fs"
 
 export function writeJSON<T>(object: T, outPath = "./out/", fileName: string) {
-	console.log(`${fileName} to ${outPath}`)
-
+	console.log(`Writing ${fileName} to ${outPath}`)
 	const json = formatKey(JSON.stringify(object, null, 2))
 	fs.writeFile(`./out/${outPath}${fileName}`, json, (err) => {
 		if (err)
