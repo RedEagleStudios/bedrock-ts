fa5efca1f80a6f8daee4611041139e9645c6f808
ink0rr <tomflynnbeltsazar@gmail.com>
2021-12-30 19:05:43 +0700

    Fix event type

--- packages/compiler/bedrock/entity/events/EventFilter.ts
+++ packages/compiler/bedrock/entity/events/EventFilter.ts
@@ -1,9 +1,8 @@
-import { EventKey } from "../BPEntity"
 import { Filters } from "../filters"
 import { FilterSubject } from "../filters/FilterSubject"
 
 export type EventFilter = {
-	event?: EventKey
+	event?: string
 	target?: FilterSubject
 	filters?: Filters
 }
