f3d3b43d39b43a6af3d50fb4c9f103caa735e684
ink0rr <tomflynnbeltsazar@gmail.com>
2022-01-05 03:48:08 +0700

    Update index.ts

---
+++ src/core/BPEntityBuilder.ts
@@ -0,0 +1,49 @@
+import { BPEntity, ComponentGroups, Events } from "../bedrock/entity/BPEntity"
+import { Components } from "../bedrock/entity/components"
+import { Identifier } from "../bedrock/identifier/Identifier"
+
+export class BPEntityBuilder {
+	constructor(
+		public identifier: Identifier,
+		private behavior: BPEntity = {
+			format_version: "1.16.0",
+			MCEntity: {
+				description: {
+					identifier: identifier,
+				},
+				components: {},
+			},
+		}
+	) {}
+
+	public build(): BPEntity {
+		return this.behavior
+	}
+
+	public setComponentGroups(component_groups: ComponentGroups) {
+		if (!this.behavior.MCEntity.component_groups) this.behavior.MCEntity.component_groups = {}
+		this.behavior.MCEntity.component_groups = {
+			...this.behavior.MCEntity.component_groups,
+			...component_groups,
+		}
+	}
+
+	public setComponents(components: Components) {
+		this.behavior.MCEntity.components = {
+			...this.behavior.MCEntity.components,
+			...components,
+		}
+	}
+
+	public setEvents(events: Events) {
+		if (!this.behavior.MCEntity.events) this.behavior.MCEntity.events = {}
+		this.behavior.MCEntity.events = {
+			...this.behavior.MCEntity.events,
+			...events,
+		}
+	}
+
+	public getComponent(component: keyof Components): unknown {
+		return this.behavior.MCEntity.components[component]
+	}
+}
