be5d3141968c5a751c15db8cd690d1cf707cb4e6
ink0rr <tomflynnbeltsazar@gmail.com>
2022-01-06 11:47:07 +0700

    Set component props to optional

--- src/bedrock/item/components/MCSeed.ts
+++ src/bedrock/item/components/MCSeed.ts
@@ -1,8 +1,8 @@
 export type MCSeedPlantAtFace = "DOWN" | "UP"
 
 export interface MCSeed {
-	crop_result: string
-	plant_at: string[]
-	plant_at_any_solid_surface: boolean
-	plant_at_face: MCSeedPlantAtFace
+	crop_result?: string
+	plant_at?: string[]
+	plant_at_any_solid_surface?: boolean
+	plant_at_face?: MCSeedPlantAtFace
 }
