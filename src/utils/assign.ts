9e87b69c95d5e95dde3133b26382d5c7071e978d
ink0rr <tomflynnbeltsazar@gmail.com>
2022-02-17 20:42:12 +0700

    Add assign function

---
+++ src/utils/assign.ts
@@ -0,0 +1,10 @@
+/**
+ * Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
+ *
+ * **Note:** This method mutates `target`
+ * @param target The target object to copy to.
+ * @param source The source object from which to copy properties.
+ */
+export function assign<T extends object>(target: NonNullable<T>, source: NonNullable<T>): NonNullable<T> {
+	return Object.assign(target, source)
+}
