3a6c74a59725d4f8aec87a174be2d1ba5b70993d
ink0rr <tomflynnbeltsazar@gmail.com>
2022-03-06 18:25:31 +0700

    Add spell effects type

--- src/bedrock/item/components/MCFood.ts
+++ src/bedrock/item/components/MCFood.ts
@@ -1,6 +1,14 @@
 import { Vector3 } from "../../../types/Vector3"
+import { SpellEffects } from "../../shared/SpellEffects"
 
 export type MCFoodCooldownType = "none" | "chorusfruit"
+
+export interface MCFoodEffect {
+	name?: SpellEffects
+	amplifier?: number
+	chance?: number
+	duration?: number
+}
 
 export type MCFoodOnUseAction = "none" | "chorus_teleport" | "suspicious_stew_effect"
 
@@ -10,7 +18,7 @@ export interface MCFood {
 	can_always_eat?: boolean
 	cooldown_time?: number
 	cooldown_type?: MCFoodCooldownType
-	effects?: string[]
+	effects?: MCFoodEffect[]
 	nutrition?: number
 	on_use_action?: MCFoodOnUseAction
 	on_use_range?: Vector3
