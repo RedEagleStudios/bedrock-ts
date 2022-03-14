caaf8a020fffa1a9570d8b2d4391b824b23f56cd
ink0rr <tomflynnbeltsazar@gmail.com>
2022-03-14 21:05:59 +0700

    Add missing files

---
+++ src/constants/paths.ts
@@ -0,0 +1,3 @@
+export const PATH_TEMP = `out/tmp.txt`
+
+export const PATH_TEMP_OLD = `out/tmp-old.txt`
---
+++ src/generator/makeAddon.ts
@@ -0,0 +1,30 @@
+import { watch } from "chokidar"
+import { existsSync, rmSync } from "fs"
+import { copySync } from "fs-extra"
+import { AddonGenerator, MCAddon } from ".."
+
+export function makeAddon(addon: MCAddon) {
+	const name = addon.packName
+	const assets = `src/assets`
+	const outDir = `out/${name}`
+	const ignore = existsSync("out")
+
+	new AddonGenerator(addon).generate()
+
+	watch(assets, {
+		ignoreInitial: ignore,
+	})
+		.on("all", (event, path) => {
+			console.log(path)
+			if (event === "add" || event === "change") {
+				const outPath = `${outDir} ${path.substring(assets.length + 1)}`
+				copySync(path, outPath)
+			}
+		})
+		.on("unlink", (path) => {
+			const outPath = `${outDir} ${path.substring(assets.length + 1)}`
+			if (existsSync(outPath)) {
+				rmSync(outPath)
+			}
+		})
+}
